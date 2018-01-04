84f8c6e239c8aa07d64da32d80d238b1c75bd911
Brock Duncan <bduncan@tkg.com>
2018-01-04 16:50:42 -0500

    adding html-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,49 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const cleanWebpackPlugin = require('clean-webpack-plugin');
-const webpackThemePath = path.join(__dirname, 'src/site/themes/hugulpack');
+const hugoThemePath = path.join(__dirname, 'src/site/themes/hugulpack');
 
 
 // clean webpack paths
 const cleanPaths = [
-    webpackThemePath + '/static/js/*',
-    webpackThemePath + '/static/css/*'
+    hugoThemePath + '/static/js/*',
+    hugoThemePath + '/static/css/*'
 ];
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: {
+        app: './src/js/app.js'
+    },
     output: {
-        path: path.join(webpackThemePath, '/static'),
+        path: path.join(hugoThemePath, '/static'),
         publicPath: '/'
     },
-      plugins: [
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]-[hash].[ext]',
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
         new cleanWebpackPlugin(cleanPaths),
         new htmlWebpackPlugin({
-            template: path.join(webpackThemePath, '/layouts/_default/baseof_temp.html'),
-            filename: path.join(webpackThemePath, '/layouts/_default/baseof.html')
+            template: path.join(hugoThemePath, '/layouts/_default/baseof_temp.html'),
+            filename: path.join(hugoThemePath, '/layouts/_default/baseof.html'),
+            chunks: 'app'
         })
-      ]
+    ]
 };
