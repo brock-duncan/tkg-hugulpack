c3b30163b64aaf32f11981a82d6d09b08ca5c3ec
Brock Duncan <bduncan@tkg.com>
2018-01-04 17:08:01 -0500

    changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,4 @@ import '../scss/styles.scss';
 // import '../site/themes/hugulpack/layouts/index.html';
 // import '../site/themes/hugulpack/static/img/madcat.jpg';
 
-console.log('webpacked!!!!');
+console.log('webpack');
